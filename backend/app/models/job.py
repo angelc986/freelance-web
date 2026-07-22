2c9356b22746fb70cb8eba80a9a7788c0574b235
angelc986 <instaworkve@gmail.com>
2026-07-21 18:26:28 -0700

    fix: import Text de SQLAlchemy

--- backend/app/models/job.py
+++ backend/app/models/job.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime, Text
 from sqlalchemy.orm import relationship
 from datetime import datetime, timezone
 from app.database import Base
